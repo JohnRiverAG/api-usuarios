53c09a54c9f8e7177d11684582edfda2ac009286
John Arango <johnunivalle@gmail.com>
2025-08-08 21:30:09 +0200

    Configuración del servidor creada

--- app.js
+++ app.js
@@ -1,2 +1,15 @@
 // Punto de entrada
 
+const express = require('express');
+const bodyParser = require('body-parser');
+const usuariosRoutes = require('./routes/usuarios');
+
+const app = express();
+const PORT = 3000;
+
+app.use(bodyParser.json());
+app.use('/api/usuarios', usuariosRoutes);
+
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
